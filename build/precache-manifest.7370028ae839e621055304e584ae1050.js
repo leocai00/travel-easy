ed7a5e77da48ac74bfb6b27483d4ffdec2c370d9
Leo Cai <leocai@leos-mbp.lan>
2019-02-04 03:03:01 -0800

    starter

---
+++ build/precache-manifest.7370028ae839e621055304e584ae1050.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "26b08b6a79645ba9fe9b2387281d32ad",
+    "url": "/travelpe-easy/static/media/yiran.26b08b6a.png"
+  },
+  {
+    "revision": "6478ac224ed4bdc1f685df8a3774cc28",
+    "url": "/travelpe-easy/static/media/purpose.6478ac22.jpg"
+  },
+  {
+    "revision": "af037b33f50a5236278976dd82d134d5",
+    "url": "/travelpe-easy/static/media/leo.af037b33.jpg"
+  },
+  {
+    "revision": "a781f8549469834dcdce374447fbedde",
+    "url": "/travelpe-easy/static/media/background.a781f854.jpg"
+  },
+  {
+    "revision": "0216d4fb0db8f98cf6e0",
+    "url": "/travelpe-easy/static/js/runtime~main.0216d4fb.js"
+  },
+  {
+    "revision": "df0ac5cd5903e464cba6",
+    "url": "/travelpe-easy/static/js/main.df0ac5cd.chunk.js"
+  },
+  {
+    "revision": "9b538593fdb846eb560a",
+    "url": "/travelpe-easy/static/js/1.9b538593.chunk.js"
+  },
+  {
+    "revision": "df0ac5cd5903e464cba6",
+    "url": "/travelpe-easy/static/css/main.b5d86845.chunk.css"
+  },
+  {
+    "revision": "00f6697ee54e7a4b4b561b61d21b0392",
+    "url": "/travelpe-easy/index.html"
+  }
+];
